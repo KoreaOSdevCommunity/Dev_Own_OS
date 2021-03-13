46a28cd9dcd0a8fd0f3e9dcc2005bc3a94e08c30
Dev.CoderK <61854396+Developer-CoderK@users.noreply.github.com>
2021-03-13 21:20:19 +0900

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -1,1 +1,18 @@
-
+function readfile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    var contenst = -1;
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                contenst = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return contenst;
+}
